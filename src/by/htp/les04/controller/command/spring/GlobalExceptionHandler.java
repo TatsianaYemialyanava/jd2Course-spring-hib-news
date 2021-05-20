e5d5391ea4aea029a2c2583326e295aba616898f
TatsianaYemialyanava <tatsiana.yemialyanava@gmail.com>
2021-05-20 14:07:40 +0300

    logger added

--- src/by/htp/les04/controller/command/spring/GlobalExceptionHandler.java
+++ src/by/htp/les04/controller/command/spring/GlobalExceptionHandler.java
@@ -3,21 +3,21 @@ package by.htp.les04.controller.command.spring;
 import static by.htp.les04.controller.command.spring.ControllerLevelConstant.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.apache.logging.log4j.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
 @Component
 	public class GlobalExceptionHandler extends SimpleMappingExceptionResolver {
-	//private static Logger logger = LogManager.getLogger();
+	private static Logger logger = LogManager.getLogger();
 	
 	@Override
     protected ModelAndView doResolveException(HttpServletRequest req, HttpServletResponse resp, Object handler,Exception ex) {
-		System.out.println(ex.getMessage());
 		ModelAndView modelAndView = new ModelAndView();
 	    modelAndView.setViewName(REDIRECT_PREFIX + REDIRECT_TO_ERROR_PAGE);
 	    modelAndView.addObject("message", ex.getMessage());
+	    logger.info("# exception occured #" + ex.getMessage());
 	    return modelAndView;
     }
 }
